ae27b30a1882590b825735b65e8f16254bd0d49c
marvinkn <marvin.nyingi97@gmail.com>
2024-06-19 16:42:12 +0300

    eureka server + product-service eureka-client registration

--- ServiceDiscovery/src/main/java/dev/marvin/ServiceDiscoveryApplication.java
+++ ServiceDiscovery/src/main/java/dev/marvin/ServiceDiscoveryApplication.java
@@ -2,14 +2,14 @@ package dev.marvin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class ProductServiceApplication {
+@EnableEurekaServer
+public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
 	}
 
 }
